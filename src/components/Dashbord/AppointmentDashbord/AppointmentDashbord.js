14fa747ecea04005183dd4e06681b0f89f419d2b
Shafin The Developer <sahariar350@gmail.com>
2021-04-12 23:53:57 +0600

    something is cooking

--- src/components/Dashbord/AppointmentDashbord/AppointmentDashbord.js
+++ src/components/Dashbord/AppointmentDashbord/AppointmentDashbord.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
-const AppointmentDashbord = () => {
+const AppointmentDashboard = () => {
+    const [date, setDate] = useState(new Date());
+
+    const onChange = (value) => {
+        setDate(value);
+    }
+    
     return (
         <div>
-            
+            <div className="col-md-4 mt-5">
+                <Calendar
+                    onChange={onChange}
+                    value={date}
+                />
+            </div>
+            <div className="col-md-4 mt-5">
+
+            </div>
         </div>
     );
 };
 
-export default AppointmentDashbord;
+export default AppointmentDashboard;
--- src/components/Dashbord/Dashbord/Dashboard.js
+++ src/components/Dashbord/Dashbord/Dashboard.js
@@ -1,27 +1,15 @@
 import React, { useState } from 'react';
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
+import AppointmentDashboard from '../AppointmentDashbord/AppointmentDashbord';
 
 const Dashboard = () => {
 
-    const [date, setDate] = useState(new Date());
-
-    const onChange = (value) => {
-        setDate(value);
-    }
     return (
         <div className="container-fluid row">
             <div className="col-md-4 bg-brand">
                 <button className="btn btn-primary">Appointment</button>
             </div>
-            <div className="col-md-4 mt-5">
-                <Calendar
-                    onChange={onChange}
-                    value={date}
-                />
-            </div>
-            <div className="col-md-4 mt-5">
-                
+            <div className="col-md-8">
+                <AppointmentDashboard></AppointmentDashboard>
             </div>
         </div>
     );
