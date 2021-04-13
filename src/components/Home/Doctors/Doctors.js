b094a5ae7b61de3a92da20cb29fc1b220b09e2fd
Shafin The Developer <sahariar350@gmail.com>
2021-04-14 05:38:09 +0600

    something is cooking

--- src/components/Home/Doctors/Doctors.js
+++ src/components/Home/Doctors/Doctors.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import Doctor from '../Doctor/Doctor';
 const Doctors = () => {
+    const [doctors, setDoctors] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/doctors')
+            .then(function (response) {
+                setDoctors(response.data);
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+            .then(function () {
+            });
+    }, [])
+
     return (
         <section className="doctors">
             <div className="container">
                 <h5 className="text-center  text-primary mb-5">Our Doctors</h5>
                 <div className="row">
-                    <Doctor/>
-                    <Doctor/>
-                    <Doctor/>
+                    {
+                        doctors.map(doctor => <Doctor doctor={doctor} />)
+                    }
                 </div>
             </div>
         </section>
